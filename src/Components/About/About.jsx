e6000f1efdbacb15ab23716fd2e0336ccb3282ef
Mukesh16407 <mukesh.16407@gmail.com>
2024-03-20 17:49:18 +0530

    latest link add

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,71 +1,71 @@
-import React from 'react';
-import { FaTwitter, FaGithub, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import { FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 import {
-    AboutDiv,
-    Column,
-    Img,
-    AboutInfo,
-    AboutInfoP,
-    HeaderUl,
-    HeaderLi,
-    JelloHori,
-} from './Styles';
+  AboutDiv,
+  Column,
+  Img,
+  AboutInfo,
+  AboutInfoP,
+  HeaderUl,
+  HeaderLi,
+  JelloHori,
+} from "./Styles";
 
-import { Container, H1 } from '../../SharedStyles/SharedStyles';
+import { Container, H1 } from "../../SharedStyles/SharedStyles";
 const socialMediaLinks = [
-    {
-        id: 0,
-        link: 'https://www.linkedin.com/in/mukesh-kumar-464273229/',
-        icon: <FaLinkedin />,
-    },
-    {
-        id: 1,
-        link: 'https://github.com/Mukesh16407',
-        icon: <FaGithub />,
-    },
-    {
-        id: 2,
-        link: 'https://twitter.com/mkvermashk',
-        icon: <FaTwitter />,
-    },
-    
+  {
+    id: 0,
+    link: "https://www.linkedin.com/in/mukesh-kumar-464273229/",
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 1,
+    link: "https://github.com/Mukesh16407",
+    icon: <FaGithub />,
+  },
+  {
+    id: 2,
+    link: "https://twitter.com/mkvermashk",
+    icon: <FaTwitter />,
+  },
 ];
 const About = () => {
-    return (
-        <Container className='about'>
-            <AboutDiv>
-                <Column>
-                    <Img src='images/my-avatar.png' alt='my image' />
-                </Column>
-                <Column>
-                    <H1>About Me</H1>
-                    <AboutInfo>
-                        <AboutInfoP>
-                        A passionate aspiring Full Stack Developer skilled in MERN stack, moulded and shaped by Masai School's numerous training.
-                        Passion and diligence are two traits that I believe most reflect me as an individual. I’ve been faced with many challenges.
-                        Without enthusiasm, patience and some degree of positivity, we cannot bring about the change that is needed to a better life!
-                        Actively ready to join in a great lively team of a good start-up to adapt me in any situation and environment with ease and perform the best
-                        </AboutInfoP>
-                       
-                    </AboutInfo>
-                    <HeaderUl>
-                        {socialMediaLinks.map((item) => (
-                            <JelloHori key={item.id}>
-                                <a
-                                    href={item.link}
-                                    target='_blank'
-                                    rel='noopener noreferrer'
-                                >
-                                    <HeaderLi>{item.icon}</HeaderLi>
-                                </a>
-                            </JelloHori>
-                        ))}
-                    </HeaderUl>
-                </Column>
-            </AboutDiv>
-        </Container>
-    );
+  return (
+    <Container className="about">
+      <AboutDiv>
+        <Column>
+          <Img src="images/my-avatar.png" alt="my image" />
+        </Column>
+        <Column>
+          <H1>About Me</H1>
+          <AboutInfo>
+            <AboutInfoP>
+              With 1.2 years of experience in software engineering, expertise
+              lies in application layers, presentation layers, and databases.
+              Proficient in problem-solving and possessing a strong aptitude,
+              focus remains on continuous improvement. Thriving in dynamic
+              environments, collaboration with multidisciplinary teams fosters
+              innovation. Passionate about skill refinement and staying updated
+              on industry trends. Committed to delivering high-quality software
+              products aligning with user requirements and business objectives.
+              Embracing challenges and opportunities for growth, aim is to
+              positively contribute to projects and organizations engaged with.
+            </AboutInfoP>
+          </AboutInfo>
+          <HeaderUl>
+            {socialMediaLinks.map((item) => (
+              <JelloHori key={item.id}>
+                <a href={item.link} target="_blank" rel="noopener noreferrer">
+                  <HeaderLi>{item.icon}</HeaderLi>
+                </a>
+              </JelloHori>
+            ))}
+          </HeaderUl>
+        </Column>
+      </AboutDiv>
+    </Container>
+  );
 };
 
 export default About;
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,178 +1,169 @@
-import React from 'react';
-import { FaGithub, FaLink, FaNodeJs } from 'react-icons/fa';
+import React from "react";
+import { FaGithub, FaLink, FaNodeJs } from "react-icons/fa";
 import {
-    ProjectDiv,
-    Row,
-    ServiceBox,
-    ServiceBoxHeader,
-    ServiceBoxP,
-    A,
-    BoxDiv,
-} from './Styles';
-import { Container, Common, H1 } from '../../SharedStyles/SharedStyles';
-import { FaHtml5, FaReact, FaJs, FaCss3 } from 'react-icons/fa';
-import { SiMongodb, SiRedux, SiHeroku } from 'react-icons/si';
+  ProjectDiv,
+  Row,
+  ServiceBox,
+  ServiceBoxHeader,
+  ServiceBoxP,
+  A,
+  BoxDiv,
+} from "./Styles";
+import { Container, Common, H1 } from "../../SharedStyles/SharedStyles";
+import { FaHtml5, FaReact, FaJs, FaCss3 } from "react-icons/fa";
+import { SiMongodb, SiRedux, SiHeroku } from "react-icons/si";
 
-
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const Projects = () => {
-    const projects = [
-        {
-            id: 1,
-            project_name: 'Derm Stores',
-            image: 'projectImages/dermstore.png',
-            project_desc:
-                'This is an E-Commerce website which was created in 1999 by a dermatologist who wanted to better serve his patients with easier access to the treatments their skin needed. hard-to-find spa care and natural beauty brands, professional hair care and makeup products, and specialty brands from around the world.',
-            deploy_link: 'https://dermstore-u2.netlify.app/',
-            repo_link: 'https://github.com/Mukesh16407/Dermstor_Clone',
-            tech_stack: [<FaHtml5 />, <FaCss3 />, <FaJs />],
-        },
-        {
-            id: 2,
-            project_name: 'Kickstart',
-            image: '/ProjectImages/kickstarter-image.png',
-            project_desc:
-                'Kickstarter PBC is a funding platform for creative projects. Everything from films, games, and music to art, design, and technology.It  is full of ambitious, innovative, and imaginative ideas that are brought to life through the direct support of others.',
-            deploy_link: 'https://kickstarter-psi.vercel.app/',
-            repo_link: 'https://github.com/Ravi-Krt-Patel/KICKSTARTER',
-            tech_stack: [
-                <FaHtml5 />,
-                <FaCss3 />,
-                <FaJs />,
-                <SiHeroku />,
-                <FaNodeJs />,
-                <FaReact/>,
-                <SiRedux />,
-                <SiMongodb />,
-            ],
-        },
-        {
-            id: 3,
-            project_name: 'ebay',
-            image: '/projectImages/ebay1.png',
-            project_desc:
-                "eBay Inc.which is an American multinational e-commerce corporation based in San Jose, California, that facilitates consumer-to-consumer and business-to-consumer sales through its website.eBay is a multibillion-dollar business with operations in about 32 countries, as of 2019.",
-            deploy_link: 'https://unit4-ebay.netlify.app/',
-            repo_link: 'https://github.com/Sweta-Swarnakar/ebay-clone',
-            tech_stack: [
-                // <SiMaterialui />,
-                <FaHtml5 />,
-                <FaCss3 />,
-                <FaJs />,
-                <SiMongodb />,
-                 //<SiExpress />,
-                <FaJs />,
-                <FaNodeJs />,
-            ],
-        },
-        {
-            id: 4,
-            project_name: 'Movie-App',
-            image: '/projectImages/entertenment.png',
-            project_desc:
-                'A Movie platform that allows users to search movies, filter the movies show all details of movies show all crew of the movies and also show trailer of selected movie',
-            deploy_link: 'https://celebrated-swan-2458ec.netlify.app/',
-            repo_link: 'https://github.com/Mukesh16407/EntertenmentHub',
-            tech_stack: [
-               
-                <FaJs />,
-                <FaReact/>,
-                <FaHtml5 />,
-                <FaCss3 />,
-            ],
-        },
-        {
-            id: 5,
-            project_name: 'MERN Stack Project',
-            image: '/projectimages/mernhomepage.png',
-            project_desc:
-                'This is mern project with all advance functionality like serach, filter,pagination  with all curd operation.',
-            deploy_link: 'https://willowy-pasca-91d228.netlify.app/',
-            repo_link: 'https://github.com/Mukesh16407/frontend-primeproject',
-            tech_stack: [ <FaJs />, <FaHtml5 />, <FaCss3 />,<FaReact/>,<FaNodeJs />,<SiMongodb />],
-        },
-        {
-            id: 6,
-            project_name: 'Doctor Appointment Booking App',
-            image: '/projectImages/doctorAppointment.png',
-            project_desc:
-                'Separate User Interfaces for Users, Admins, Doctors and also get Notifications in Users, Admin, and Doctors for any update',
-            deploy_link: 'https://doctorappointment2.herokuapp.com/login',
-            repo_link: 'https://github.com/Mukesh16407/doctorappointment',
-            tech_stack: [ <FaHtml5 />,
-            <FaJs />,
-            <SiHeroku />,
-            <FaNodeJs />,
-            <FaReact/>,
-            <SiRedux />,
-            <SiMongodb />,],
-        },
-    ];
+  const projects = [
+    {
+      id: 1,
+      project_name: "Derm Stores",
+      image: "projectImages/dermstore.png",
+      project_desc:
+        "This is an E-Commerce website which was created in 1999 by a dermatologist who wanted to better serve his patients with easier access to the treatments their skin needed. hard-to-find spa care and natural beauty brands, professional hair care and makeup products, and specialty brands from around the world.",
+      deploy_link: "https://dermstore-u2.netlify.app/",
+      repo_link: "https://github.com/Mukesh16407/Dermstor_Clone",
+      tech_stack: [<FaHtml5 />, <FaCss3 />, <FaJs />],
+    },
+    {
+      id: 2,
+      project_name: "Kickstart",
+      image: "/ProjectImages/kickstarter-image.png",
+      project_desc:
+        "Kickstarter PBC is a funding platform for creative projects. Everything from films, games, and music to art, design, and technology.It  is full of ambitious, innovative, and imaginative ideas that are brought to life through the direct support of others.",
+      deploy_link: "https://kickstarter-psi.vercel.app/",
+      repo_link: "https://github.com/Ravi-Krt-Patel/KICKSTARTER",
+      tech_stack: [
+        <FaHtml5 />,
+        <FaCss3 />,
+        <FaJs />,
+        <SiHeroku />,
+        <FaNodeJs />,
+        <FaReact />,
+        <SiRedux />,
+        <SiMongodb />,
+      ],
+    },
+    {
+      id: 3,
+      project_name: "ebay",
+      image: "/projectImages/ebay1.png",
+      project_desc:
+        "eBay Inc.which is an American multinational e-commerce corporation based in San Jose, California, that facilitates consumer-to-consumer and business-to-consumer sales through its website.eBay is a multibillion-dollar business with operations in about 32 countries, as of 2019.",
+      deploy_link: "https://unit4-ebay.netlify.app/",
+      repo_link: "https://github.com/Sweta-Swarnakar/ebay-clone",
+      tech_stack: [
+        // <SiMaterialui />,
+        <FaHtml5 />,
+        <FaCss3 />,
+        <FaJs />,
+        <SiMongodb />,
+        //<SiExpress />,
+        <FaJs />,
+        <FaNodeJs />,
+      ],
+    },
+    {
+      id: 4,
+      project_name: "Movie-App",
+      image: "/projectImages/entertenment.png",
+      project_desc:
+        "A Movie platform that allows users to search movies, filter the movies show all details of movies show all crew of the movies and also show trailer of selected movie",
+      deploy_link: "https://celebrated-swan-2458ec.netlify.app/",
+      repo_link: "https://github.com/Mukesh16407/EntertenmentHub",
+      tech_stack: [<FaJs />, <FaReact />, <FaHtml5 />, <FaCss3 />],
+    },
+    {
+      id: 5,
+      project_name: "MERN Stack Project",
+      image: "/projectimages/mernhomepage.png",
+      project_desc:
+        "This is mern project with all advance functionality like serach, filter,pagination  with all curd operation.",
+      deploy_link: "https://willowy-pasca-91d228.netlify.app/",
+      repo_link: "https://github.com/Mukesh16407/frontend-primeproject",
+      tech_stack: [
+        <FaJs />,
+        <FaHtml5 />,
+        <FaCss3 />,
+        <FaReact />,
+        <FaNodeJs />,
+        <SiMongodb />,
+      ],
+    },
+    {
+      id: 6,
+      project_name: "Doctor Appointment Booking App",
+      image: "/projectImages/doctorAppointment.png",
+      project_desc:
+        "Separate User Interfaces for Users, Admins, Doctors and also get Notifications in Users, Admin, and Doctors for any update",
+      deploy_link: "https://doctorappointment2.herokuapp.com/login",
+      repo_link: "onlineappointment.netlify.app/",
+      tech_stack: [
+        <FaHtml5 />,
+        <FaJs />,
+        <SiHeroku />,
+        <FaNodeJs />,
+        <FaReact />,
+        <SiRedux />,
+        <SiMongodb />,
+      ],
+    },
+  ];
 
-    return (
-        <Container className='projects'>
-            <ProjectDiv>
-                <Common>
-                    <H1>Projects</H1>
-                </Common>
-                <Row >
-                    {projects.map((item) => (
-                        <BoxDiv key={uuidv4()}>
-                            <ServiceBox>
-                                <img
-                                    src={item.image}
-                                    alt='icon'
-                                    style={{ width: '100%' }}
-                                />
-                                <div style={{ margin: '25px' }}>
-                                    <ServiceBoxHeader
-                                        style={{ textAlign: 'center' }}
-                                    >
-                                        {item.project_name}
-                                    </ServiceBoxHeader>
-                                    <ServiceBoxP>
-                                        {item.project_desc}
-                                    </ServiceBoxP>
-                                    {/* <a href="">Read more</a> */}
-                                    <div
-                                        style={{
-                                            margin: '20px',
-                                            display: 'flex',
-                                            justifyContent: 'center',
-                                        }}
-                                    >
-                                        {item.tech_stack.map((item) => (
-                                            <div
-                                                key={uuidv4()}
-                                                className='iconsTechStack'
-                                            >
-                                                {item}
-                                            </div>
-                                        ))}
-                                    </div>
-                                    <A
-                                        href={item.repo_link}
-                                        target='_blank'
-                                        rel='noopener noreferrer'
-                                    >
-                                        <FaGithub /> Code
-                                    </A>
-                                    <A
-                                        href={item.deploy_link}
-                                        target='_blank'
-                                        rel='noopener noreferrer'
-                                    >
-                                        <FaLink /> Demo
-                                    </A>
-                                </div>
-                            </ServiceBox>
-                        </BoxDiv>
+  return (
+    <Container className="projects">
+      <ProjectDiv>
+        <Common>
+          <H1>Projects</H1>
+        </Common>
+        <Row>
+          {projects.map((item) => (
+            <BoxDiv key={uuidv4()}>
+              <ServiceBox>
+                <img src={item.image} alt="icon" style={{ width: "100%" }} />
+                <div style={{ margin: "25px" }}>
+                  <ServiceBoxHeader style={{ textAlign: "center" }}>
+                    {item.project_name}
+                  </ServiceBoxHeader>
+                  <ServiceBoxP>{item.project_desc}</ServiceBoxP>
+                  {/* <a href="">Read more</a> */}
+                  <div
+                    style={{
+                      margin: "20px",
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {item.tech_stack.map((item) => (
+                      <div key={uuidv4()} className="iconsTechStack">
+                        {item}
+                      </div>
                     ))}
-                </Row>
-            </ProjectDiv>
-        </Container>
-    );
+                  </div>
+                  <A
+                    href={item.repo_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaGithub /> Code
+                  </A>
+                  <A
+                    href={item.deploy_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaLink /> Demo
+                  </A>
+                </div>
+              </ServiceBox>
+            </BoxDiv>
+          ))}
+        </Row>
+      </ProjectDiv>
+    </Container>
+  );
 };
 
 export default Projects;
